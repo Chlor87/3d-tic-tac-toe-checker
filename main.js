0e784eb02f66e0731e3f8a2cc6b49b612513be72
Kuba Kutiak <jan.kutiak@iqvia.com>
2022-07-25 22:55:54 +0200

    initial

---
+++ main.js
@@ -0,0 +1,79 @@
+/**
+ * make all Math static methods accessible without namespace
+ */
+Object.getOwnPropertyNames(Math).forEach(k => (globalThis[k] = Math[k]));
+
+import { colinear } from './math.js';
+
+const X = Symbol('X');
+const O = Symbol('O');
+const _ = Symbol('_');
+
+const vec3 = (x, y, z) => [x, y, z];
+
+/**
+ * 3D board indexed [x][y][z]
+ * prettier-ignore
+ */
+const board = [
+  [
+    [X, _, _],
+    [O, _, _],
+    [_, _, _]
+  ],
+  [
+    [_, _, _],
+    [O, X, _],
+    [_, _, _]
+  ],
+  [
+    [_, _, _],
+    [O, _, _],
+    [_, _, X]
+  ]
+];
+
+/**
+ * get all coordinates for some symbol
+ */
+const getCoordsFor = (board, symbol) =>
+  board.reduce(
+    (p, c, x) =>
+      c.reduce(
+        (p, c, y) =>
+          c.reduce((p, c, z) => (c === symbol ? [...p, vec3(x, y, z)] : p), p),
+        p
+      ),
+    []
+  );
+
+// nCr - choose k-sized combinations from xs, or n choose k
+const choose = (xs, k) =>
+  k <= 0 || k > xs.length
+    ? []
+    : k === xs.length
+    ? [xs]
+    : k === 1
+    ? xs.map(x => [x])
+    : Array.from({ length: xs.length - k + 1 }).reduce(
+        (p, _, i) => [
+          ...p,
+          ...choose(xs.slice(i + 1), k - 1).map(ys => [xs[i], ...ys])
+        ],
+        []
+      );
+
+/**
+ * 1. get coordinates for the specified symbol
+ * 2. generate all possible triples of coordinates without repetition (nCr)
+ * 3. check if any triple is colinear; if yes, the symbol wins
+ */
+const hasWinner = (board, symbol) =>
+  choose(getCoordsFor(board, symbol), 3).some(ps => colinear(...ps));
+
+const main = () => {
+  console.log(hasWinner(board, X));
+  console.log(hasWinner(board, O));
+};
+
+main();
---
+++ math.js
@@ -0,0 +1,20 @@
+const sub = (a, b) => {
+  const scalar = typeof b === 'number';
+  return a.map((c, i) => c - (scalar ? b : b[i]));
+};
+const dot = (a, b) => a.reduce((p, c, i) => p + c * b[i], 0);
+const mag = a => Math.hypot(a[0], a[1], a[2]);
+const dist = (a, b) => mag(sub(a, b));
+
+/**
+ * 3 vectors are colinear if the absolute value of the quotient
+ * of the dot product of AB and AC and the product of their magnitudes
+ * is equal to one:
+ *
+ * dot(AB, AC) / (|AB| * |AC|)| = 1
+ *
+ * 3 is an arbitrary magic constant accounting for rounding errors
+ */
+export const colinear = (a, b, c) =>
+  1 - abs(dot(sub(a, b), sub(a, c)) / (dist(a, b) * dist(a, c))) <
+  3 * Number.EPSILON;
